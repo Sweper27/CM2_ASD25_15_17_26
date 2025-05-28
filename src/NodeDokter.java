d5c599e1328c27fbe1154cd7283e513ac4eae8d2
muhammad hasbi ashiddiqi <muhammadhasbiasidiki175@gmail.com>
2025-05-28 08:53:13 +0700

    nambahdoktor

---
+++ src/NodeDokter.java
@@ -0,0 +1,12 @@
+public class NodeDokter {
+    Dokter data;
+    NodeDokter prev,next;
+    
+    public NodeDokter(Dokter data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+
